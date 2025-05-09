7dc3bfc9698183c8eb565453f5274270a31cc9ee
CaioHPerlin <caiohperlin@gmail.com>
2025-05-09 15:20:40 -0400

    chore: organize imports

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskFilterDto } from './dto/task-filter.dto';
