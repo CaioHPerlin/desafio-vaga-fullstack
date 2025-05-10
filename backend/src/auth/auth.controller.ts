6b61f2b7e3a28b7997f50eaadc3e7bab837a5a1b
CaioHPerlin <caiohperlin@gmail.com>
2025-05-10 07:53:09 -0400

    feat: (back) add getProfile route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   Post,
   Req,
@@ -9,7 +10,9 @@ import {
 import { Request } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { RegisterDto } from 'src/auth/dto/register.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,12 +21,18 @@ export class AuthController {
   @HttpCode(200)
   @Post('login')
   @UseGuards(LocalAuthGuard)
-  async login(@Req() req: Request) {
+  async login(@Req() req: { user: User }) {
     return this.authService.login(req.user);
   }
 
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto.email, registerDto.password);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Req() req: Request) {
+    return req.user;
   }
 }
--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -2,6 +2,11 @@ import { User } from 'src/users/entities/user.entity';
 
 declare module 'express' {
   interface Request {
-    user: User;
+    user: {
+      email: string;
+      sub: number;
+      iat: number;
+      exp: number;
+    };
   }
 }
