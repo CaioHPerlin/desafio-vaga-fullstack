48e2427e4d7ec5774231ee90e015d9efef2797e8
CaioHPerlin <caiohperlin@gmail.com>
2025-05-10 08:06:53 -0400

    feat: implement api error handling

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_ERROR_MESSAGES: Record<number, string> = {
+  500: "Ocorreu um erro no servidor.",
+  400: "Entrada inválida. Por favor, verifique os dados e tente novamente.",
+  403: "Você não tem permissão para executar essa ação.",
+  999: "Ocorreu um erro inesperado. Tente novamente mais tarde.",
+};
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { DEFAULT_ERROR_MESSAGES } from "@/lib/constants";
+import { isAxiosError } from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getCustomErrorCode(error: unknown): number {
+  return isAxiosError(error) ? error.response?.status ?? 999 : 999;
+}
+
+export function handleApiError(
+  error: unknown,
+  errorMessages?: Record<number, string>
+): never {
+  errorMessages = {
+    ...DEFAULT_ERROR_MESSAGES,
+    ...errorMessages,
+  };
+
+  const code = getCustomErrorCode(error);
+  const message = errorMessages[code] || errorMessages[999];
+  throw new Error(message);
 }
